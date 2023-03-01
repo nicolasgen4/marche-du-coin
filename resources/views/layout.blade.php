4a7b76954bd8c4467bfefd08dc2da239cf7d3c47
nicolasgen4 <121958459+nicolasgen4@users.noreply.github.com>
2023-03-01 16:37:23 +0100

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="{{asset('css/normalize.css')}}" media="screen">
     <link rel="stylesheet" href="{{asset('css/global.css')}}" media="screen">
     <link rel="stylesheet" href="{{asset('css/layout.css')}}" media="screen">
+    <link rel="stylesheet" href="{{asset('css/composants.css')}}" media="screen">
     <link rel="stylesheet" href="{{asset('css/sections.css')}}" media="screen">
 </head>
 
@@ -19,7 +20,7 @@
             <a href="/" class="logo">
                 <figure class="row-center">
                     <title>Le marché du coin</title>
-                    <svg width="48" height="41" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg width="48" height="41" fill="#2D2D2D" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M35.367 15.078 25.811.908C25.396.301 24.698 0 24 0s-1.396.302-1.81.929l-9.557 14.149H2.182c-1.2 0-2.182.972-2.182 2.16 0 .195.022.389.087.583L5.63 37.846A4.332 4.332 0 0 0 9.82 41h28.363a4.378 4.378 0 0 0 4.21-3.154l5.542-20.025.066-.583c0-1.188-.982-2.16-2.182-2.16h-10.45Zm-17.912 0L24 5.573l6.546 9.505H17.455ZM24 32.359c-2.4 0-4.364-1.944-4.364-4.32 0-2.376 1.964-4.32 4.364-4.32s4.364 1.944 4.364 4.32c0 2.376-1.964 4.32-4.364 4.32Z" />
                     </svg>
@@ -28,8 +29,8 @@
             </a>
             <ul class="row-center">
                 <li>Vous êtes un producteur du Rouillacais ?</li>
-                <li><a href="/">Je vends mes produits</a></li>
-                <li><a href="/">Je gère mes annonces</a></li>
+                <li><a href="/" class="bouton">Je vends mes produits</a></li>
+                <li><a href="/" class="bouton">Je gère mes annonces</a></li>
             </ul>
         </nav>
         <section class="hero column-center">
@@ -50,8 +51,8 @@
     <footer>
         <section class="wrapper row-center">
             <h5>Le marché du coin</h5>
-            <nav>
-                <ul>
+            <nav class="row-center">
+                <ul class="row-center">
                     <li><a href="/">Mentions Légales</a></li>
                     <li><a href="/">Politique de confidentialité</a></li>
                 </ul>

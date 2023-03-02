7bf6c5f629dfceb04ec6c36c9d1bf88f03403f0c
nicolasgen4 <121958459+nicolasgen4@users.noreply.github.com>
2023-03-02 16:07:28 +0100

    Update vente-carte.blade.php

--- resources/views/components/vente-carte.blade.php
+++ resources/views/components/vente-carte.blade.php
@@ -2,10 +2,10 @@
 
 <section class="item column-btw-center">
     <img src="images/{{$vente->image}}" alt="{{$vente->titre}}" loading="lazy">
-    <h2>{{$vente->titre}}</h2>
-    <section>
-        <h3>{{$vente->debut_vente}}</h3>
-        <h4>{{$vente->localisation}}</h4>
+    <h2>{{mb_strimwidth(ucfirst($vente->titre), 0, 60, '...')}}</h2>
+    <section class="informations column-btw">
+        <h3>Début : {{$vente->debut_vente}}</h3>
+        <h4>Localité : {{$vente->localisation}}</h4>
         <x-vente-etiquettes :etiquettesSpv="$vente->etiquettes" />
         <p>{{mb_strimwidth(ucfirst($vente->description), 0, 160, '...')}}</p>
     </section>

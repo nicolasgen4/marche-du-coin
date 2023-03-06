92bc22a690cac500efe786357c26b23b024803bd
nicolasgen4 <121958459+nicolasgen4@users.noreply.github.com>
2023-03-06 18:10:41 +0100

    Update Ventes.php

--- app/Models/Ventes.php
+++ app/Models/Ventes.php
@@ -8,4 +8,43 @@ use Illuminate\Database\Eloquent\Model;
 class Ventes extends Model
 {
     use HasFactory;
+
+    public function scopeFilter($query, array $filters)
+    {
+        if ($filters['tag'] ?? false) {
+            $query->where(
+                'etiquettes',
+                'like',
+                '%' . request('tag') . '%'
+            );
+        }
+
+        if ($filters['search'] ?? false) {
+            $query->where(
+                'titre',
+                'like',
+                '%' . request('search') . '%'
+            )
+            ->orWhere(
+                'etiquettes',
+                'like',
+                '%' . request('search') . '%'
+            )
+            ->orWhere(
+                'description',
+                'like',
+                '%' . request('search') . '%'
+            )
+            ->orWhere(
+                'vendeur',
+                'like',
+                '%' . request('search') . '%'
+            )
+            ->orWhere(
+                'localisation',
+                'like',
+                '%' . request('search') . '%'
+            );
+        }
+    }
 }

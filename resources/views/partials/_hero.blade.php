3bfad5a30933dc2e02832fbbffd91b8f64afdec8
nicolasgen4 <121958459+nicolasgen4@users.noreply.github.com>
2023-03-08 15:52:17 +0100

    Add files via upload

---
+++ resources/views/partials/_hero.blade.php
@@ -0,0 +1,7 @@
+<section class="hero column-center">
+    <h1>Les produits frais des agriculteurs du Rouillacais</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur a aliquam cum voluptate corporis!
+        Et, optio! Debitis praesentium doloribus, earum incidunt porro labore veniam exercitationem a quos rem,
+        ipsam iste.</p>
+    <x-vente-search :search />
+</section>

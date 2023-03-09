c304b5e52bc6a5547fb144b1911d3353d8154bef
nicolasgen4 <121958459+nicolasgen4@users.noreply.github.com>
2023-03-09 16:09:58 +0100

    Update VentesController.php

--- app/Http/Controllers/VentesController.php
+++ app/Http/Controllers/VentesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Ventes;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class VentesController extends Controller
 {
@@ -26,5 +27,28 @@ class VentesController extends Controller
         return view('ventes.show', [
             'vente' => $vente
         ]);
+    }
+
+    //Affiche le formulaire de création
+    public function create()
+    {
+        return view('ventes.create');
+    }
+
+    //Stocke une vente
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'titre' => ['required', Rule::unique('ventes', 'titre')],
+            'tags' => 'required',
+            'vendeur' => 'required',
+            'localisation' => 'required',
+            'email' => ['required', 'email'],
+            'description' => 'required'
+        ]);
+
+        Ventes::create($formFields);
+
+        return redirect('/');
     }
 }

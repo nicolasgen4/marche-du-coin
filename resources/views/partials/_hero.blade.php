119552041c553ceb3bc152ccc4a2467f2b01072a
nicolasgen4 <121958459+nicolasgen4@users.noreply.github.com>
2023-03-13 16:22:58 +0100

    Update _hero.blade.php

--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -3,5 +3,5 @@
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur a aliquam cum voluptate corporis!
         Et, optio! Debitis praesentium doloribus, earum incidunt porro labore veniam exercitationem a quos rem,
         ipsam iste.</p>
-    <x-vente-search :search />
+    <x-vente-search/>
 </section>

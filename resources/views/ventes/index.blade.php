936fd4dd1ed66646dcc54bad43461f076540c33e
nicolasgen4 <121958459+nicolasgen4@users.noreply.github.com>
2023-03-17 17:13:24 +0100

    Update index.blade.php

--- resources/views/ventes/index.blade.php
+++ resources/views/ventes/index.blade.php
@@ -1,18 +1,22 @@
 <x-layout>
 
-@unless (count($ventes) == 0)
+    @unless (count($ventes) == 0)
 
-<div id="ventes" class="items row-wrap-center">
-    @foreach($ventes as $vente)
-    <x-vente-carte :vente="$vente" />
-    @endforeach
-</div>
+    <div id="ventes" class="items row-wrap-center">
+        @foreach($ventes as $vente)
+        <x-vente-carte :vente="$vente" />
+        @endforeach
+    </div>
 
-@else
-<section>
-    <h2>Aucune vente disponible</h2>
-</section>
+    @else
+    <section>
+        <h2>Aucune vente disponible</h2>
+    </section>
 
-@endunless
+    @endunless
+
+
+    {{$ventes->links()}}
+
 
 </x-layout>

8ebcadcdbb4505f0fd92247a5913adc61d71b011
nicolasgen4 <121958459+nicolasgen4@users.noreply.github.com>
2023-04-06 16:07:25 +0200

    Update 2023_02_10_153134_create_offres_table.php

--- database/migrations/2023_02_10_153134_create_offres_table.php
+++ database/migrations/2023_02_10_153134_create_offres_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('ventes', function (Blueprint $table) {
             $table->id();
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->string('titre')->unique();
             $table->string('etiquettes');
             $table->string('vendeur');

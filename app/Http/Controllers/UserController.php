a109ea28596c931418dbda54427f561d0ec85193
nicolasgen4 <121958459+nicolasgen4@users.noreply.github.com>
2023-04-25 15:20:12 +0200

    Add files via upload

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+
+class UserController extends Controller
+{
+    //Affiche le formulaire d'enregistrement
+    public function create()
+    {
+        return view('users.register');
+    }
+
+    //Créer un utilisateur
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'name' => ['required', 'min:3'],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        //Hash
+        $formFields['password'] = bcrypt($formFields['password']);
+
+        //Crée l'utilisateur
+        $user = User::create($formFields);
+
+        //Connexion
+        auth()->login($user);
+
+        return redirect('/')->with('message', 'Utilisateur créé avec succès');
+    }
+}

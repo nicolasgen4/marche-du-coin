29e8a5cfb5a67c2c33a94fa645703aedd0ee1d84
nicolasgen4 <121958459+nicolasgen4@users.noreply.github.com>
2023-04-25 18:22:12 +0200

    Update 2023_02_10_153134_create_offres_table.php

--- database/migrations/2023_02_10_153134_create_offres_table.php
+++ database/migrations/2023_02_10_153134_create_offres_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('ventes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('image')->nullable();
             $table->string('titre')->unique();
             $table->string('etiquettes');
